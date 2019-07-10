9d13d658926de40b348b0bedfb68083eaab232d1
cole peterson <cole.peterson@possible.com>
2019-07-10 13:29:38 -0700

    added class to hold static text.

---
+++ src/constants/strings.js
@@ -0,0 +1,15 @@
+export const ABOUT = `The Town Square app is used for crowd sourcing questions to be asked in a town hall setting.<br /><br />
+
+  Before a company meeting, users can submit questions and vote on other user's questions.<br />
+  The questions are displayed in a leader board and can be sorted by the associated number of votes.<br />
+  The moderator or admin for the meeting can then submit the questions with the most votes to the speaker to be answered.<br /><br />
+  
+  Hopefully by collecting questions and allowing users to vote ahead of time, the end result will be a meeting covering topics people really care about. <br /><br />
+  
+  All questions and voting is done in real time. <br />
+  Meaning users will be able to see the question vote counts go up and down as people vote as well as new questions being asked in real time.<br /><br />
+
+  Contact <a href="mailto:cole.peterson@possible">cole.peterson@possible.com</a> with any issues.
+  `;
+export const APP_NAME = 'Town Hall';
+export const APP_TAGLINE = 'Crowd Sourcing Q &amp; A';
