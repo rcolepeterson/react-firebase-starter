e9f087ab3e7751c772b5c09243e05b365da949b6
cole peterson <cole.peterson@possible.com>
2019-07-10 13:52:20 -0700

    Updated about text and margins.

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,13 +1,12 @@
 export const ABOUT = `The Town Hall app is used for crowd sourcing questions to be asked in a town hall setting.<br /><br />
 
-  Before a company meeting, users can submit questions and vote on other user's questions.<br />
-  The questions are displayed in a leader board and can be sorted by the associated number of votes.<br />
+  Before a company meeting, users can submit questions and vote on other user's questions.<br /><br />
+  The questions are displayed in a leader board and can be sorted by the associated number of votes.<br /><br />
   The moderator or admin for the meeting can then submit the questions with the most votes to the speaker to be answered.<br /><br />
   
   Hopefully by collecting questions and allowing users to vote ahead of time, the end result will be a meeting covering topics people really care about. <br /><br />
   
-  All questions and voting is done in real time. <br />
-  Meaning users will be able to see the question vote counts go up and down as people vote as well as new questions being asked in real time.<br /><br />
+  All questions and voting is done in real time. Meaning, users see question vote counts go up and down as well as see new questions being asked in real time.<br /><br />
 
   Contact <a href="mailto:cole.peterson@possible">cole.peterson@possible.com</a> with any issues.
   `;
